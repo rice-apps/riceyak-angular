e717a2ae755a87fe4bb23934293b877fd8bafced
Cade Ritter <ritter.cade@gmail.com>
2017-10-19 20:01:05 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
