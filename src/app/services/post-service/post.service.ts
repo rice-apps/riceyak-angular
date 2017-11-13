5380f98048479dd6ae521935b95444ca88a654c9
Cade Ritter <ritter.cade@gmail.com>
2017-11-13 12:24:56 -0600

    begin adding voting

--- src/app/services/post-service/post.service.ts
+++ src/app/services/post-service/post.service.ts
@@ -38,4 +38,11 @@ export class PostService {
       .then(res => res.json() as Post)
       .catch(err => console.log(err));
   }
+
+  voteOnPost(post_id: string, vote: number): Promise<any> {
+    return this.http.put(`${this.apiUrl}/posts/${post_id}`, { vote: vote }, this.jwt())
+      .toPromise()
+      .then(res => res.json() as Post)
+      .catch(err => console.log(err));
+  }
 }
