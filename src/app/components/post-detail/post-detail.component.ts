75d48ffb31a273eae8814a82133aa93e2ac94771
noushinquazi <noushinquazi@yahoo.com>
2017-11-22 23:46:36 -0600

    edit button -- edit body of post and title

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -8,61 +8,65 @@ import {User} from "../../models/user";
 import {Location} from '@angular/common';
 
 @Component({
-  selector: 'app-post-detail',
-  templateUrl: './post-detail.component.html',
-  styleUrls: ['./post-detail.component.css']
+    selector: 'app-post-detail',
+    templateUrl: './post-detail.component.html',
+    styleUrls: ['./post-detail.component.css']
 })
 export class PostDetailComponent implements OnInit {
 
-  private post: Post;
+    private post: Post;
 
-  private isMyPost: boolean = false;
+    private isMyPost: boolean = false;
+    private isEdit: boolean = false;
 
-  constructor(private postService: PostService,
-              private route: ActivatedRoute,
-              private authService: AuthService,
-              private router: Router) { }
+    constructor(private postService: PostService,
+                private route: ActivatedRoute,
+                private authService: AuthService,
+                private router: Router) { }
 
-  ngOnInit() {
-      this.route.params.subscribe(params => {
-      this.postService.getPost(params['_id'])
-        .then(post => this.initPostStatus(post));
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.postService.getPost(params['_id'])
+                .then(post => this.initPostStatus(post));
 
-    });
+        });
 
 
 
-  }
-  initPostStatus(post: Post){
-    this.post = post;
-    if (this.authService.userLoggedIn) {
-
-        if (this.authService.userLoggedIn.user.userID === this.post.author._id) {
-            this.isMyPost = true;
-        } else {
+    }
+    initPostStatus(post: Post){
+        this.post = post;
+        if (this.authService.userLoggedIn) {
+            if (this.authService.userLoggedIn.user.userID === this.post.author._id) {
+                this.isMyPost = true;
+            } else {
+                this.isMyPost = false;
+            }
+        }
+        else {
             this.isMyPost = false;
         }
     }
-    else {
-        this.isMyPost = false;
+
+    delete(){
+        this.postService.delete(this.post._id).then(post => this.router.navigate(['/posts']));
+
     }
-  }
+    comment(comment_entered: string) {
 
-  delete(){
-    this.postService.delete(this.post._id);
-      this.router.navigate(['/posts']);
-  }
-  comment(comment_entered: string) {
+        this.postService.postComment(this.post._id, comment_entered)
+            .then(post => this.post = post);
+    }
 
-      this.postService.postComment(this.post._id, comment_entered)
-          .then(post => this.post = post);
-  }
-  getPost() {
-    return this.post;
-  }
-  getIsMyPost() {
-    return this.isMyPost;
-  }
+    edit(){
+        this.isEdit=!this.isEdit;
+    }
+    submitChanges(title: string, body: string){
+        this.post.title = title.trim();
+        this.post.body = body;
+        this.edit();
+        this.postService.edit(this.post._id, this.post);
+    }
 
 
 }
